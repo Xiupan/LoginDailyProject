3b0e5f22a77600d3233a447c2d199e56a333c4c3
Alan Hong <xiupan@gmail.com>
2017-06-22 13:05:50 -0500

    Basic framework completed

---
+++ login.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const expressSession = require('express-session');
+const mustacheExpress = require('mustache-express');
+const parseUrl = require('parseurl');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+app.use(express.static('/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.listen(3000, function () {
+  console.log('Successfully started Login application!');
+});
+
+app.use(expressSession({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(function (req, res, next) {
+  var views = req.session.views
+
+  if (!views) {
+    views = req.session.views = {}
+  }
+
+  var pathname = parseurl(req).pathname
+
+  views[pathname] = (views[pathname] || 0) + 1
+
+  next()
+})
+
+app.get('/', function(request, response){
+  response.render('index');
+})
+
+app.get('/foo', function (req, res, next) {
+  res.send('you viewed this page ' + req.session.views['/foo'] + ' times')
+})
+
+app.get('/bar', function (req, res, next) {
+  res.send('you viewed this page ' + req.session.views['/bar'] + ' times')
+})
